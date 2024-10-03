1cbfbbc0361f87bfdf6380ad196759d36136b38d
niko <nrsanchez@fi.uba.ar>
2024-10-03 03:21:35 -0300

    creando boceto de pagina portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nrsanchez.github.io',
-    base: 'portfolio',
+  site: 'https://nrsanchez.github.io',
+  base: 'portfolio',
+  integrations: [tailwind()],
 });
